f2bf53abb52f07686ba0aec682c00ba446c6d867
jayanthee <pjayam.thee@gmail.com>
2013-11-26 13:10:01 +0530

    java sample from maven

---
+++ src/main/java/org/krams/tutorial/controller/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.krams.tutorial.controller;
+
+/**
+ *
+ * @author logic
+ */
+public class NewClass {
+    
+}
